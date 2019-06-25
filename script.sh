0bd91bb5128de4a8d2c8d502fcb4b7399b61c0b0
Anshul Garg <45210080+garganshul108@users.noreply.github.com>
2019-06-26 02:07:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,7 +12,15 @@ while IFS= read -r line; do
     then
         v1=$line
         echo $count : $v2
-        curl $v1 -H 'Accept-Encoding: identity;q=1, *;q=0' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/75.0.3770.90 Chrome/75.0.3770.90 Safari/537.36' -H 'Accept: */*' -H 'Connection: keep-alive' -H 'Range: bytes=0-' --compressed --output $v2
+        curl $v1 \
+        -H 'Accept-Encoding: identity;q=1, *;q=0' \
+        -H 'Accept-Language: en-US,en;q=0.9' \
+        -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/75.0.3770.90 Chrome/75.0.3770.90 Safari/537.36' \
+        -H 'Accept: */*' \
+        -H 'Connection: keep-alive' \
+        -H 'Range: bytes=0-' \
+        --compressed \
+        --output $v2
         # curl $v1 --output $v2
     fi
 
