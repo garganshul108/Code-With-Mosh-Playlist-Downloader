cae56b3d39fc633f61e5bf4edd4051d7a6e48057
garganshul108 <sci.agarg@gmail.com>
2019-06-27 14:13:39 +0530

    scrape login successfully

--- tt.py
+++ tt.py
@@ -1,3 +1,38 @@
 import requests
 from lxml import html
-session_requests = requests.session()
+
+USERNAME = "sci.agarg@gmail.com"
+PASSWORD = "asdfghjkl11111"
+
+LOGIN_URL = "https://sso.teachable.com/secure/146684/users/sign_in?clean_login=true&reset_purchase_session=1"
+URL = "https://codewithmosh.com/courses/the-complete-node-js-course/lectures/4509036"
+
+def main():
+    session_requests = requests.session()
+
+    # Get login csrf token
+    result = session_requests.get(LOGIN_URL)
+    tree = html.fromstring(result.text)
+    authenticity_token = list(set(tree.xpath("//meta[@name='csrf-token']/@content")))[0]
+    # print(authenticity_token)
+
+    # Create payload
+    payload = {
+        "user[school_id]": "146684",
+        "user[email]": USERNAME, 
+        "user[password]": PASSWORD, 
+        "csrf-token": authenticity_token
+    }
+
+    # Perform login
+    result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+
+    # Scrape url
+    result = session_requests.get(URL, headers = dict(referer = URL))
+    tree = html.fromstring(result.content)
+    # bucket_names = tree.xpath("//div[@class='repo-list--repo']/a/text()")
+    print(result.content)
+    # print(tree)
+
+if __name__ == '__main__':
+    main()
